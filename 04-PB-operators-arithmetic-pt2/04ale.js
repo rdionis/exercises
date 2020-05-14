2310814693b46e06e333be052028a02996f5d234
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-14 15:42:50 +0200

    04 ale

---
+++ 04-PB-operators-arithmetic-pt2/04ale.js
@@ -0,0 +1,40 @@
+let tiX = 15;
+console.log(6450/tiX);
+//430
+let inCome = 500;
+console.log(inCome * 52.14);
+//26,070
+let percent = 17;
+let total = 30;
+console.log(percent/30 *100);
+//56.666
+let side = 4.75;
+console.log(side*4);
+//19
+let triOne = 5;
+let triTwo = 6; 
+let triThree = 7;
+console.log(triOne+triTwo+triThree);
+//18
+let cube = 9;
+console.log(cube*cube*cube);
+//729
+let result = 17/30 * 100;
+console.log(result);
+//
+let bills = 22.35 + (10/100 * 22.35) + 26.67 + (15/100 * 26.67) + 35.92 + (20/100 * 35.92);
+console.log(bills);
+//98.3595
+let mean =  (8+6+5+9+8+2+1+8.5+7+4) / 10;
+console.log(mean);
+//5.85
+let sixthTest = 85 * 6;
+console.log(sixthTest);
+console.log(sixthTest -70 -75 -80 -90 -100);
+//95
+let min = (9*80) - (8*78);
+console.log(min);
+//96% min
+
+
+
--- PB-language-statements/hello.js
+++
@@ -1,6 +0,0 @@
-console.log"hello world");
-// console.log("this is my first js file");
-
-/* the statement doesn't print/run.
-there is an error: 
-SYNTAXERROR: unexpected string @ line 1
