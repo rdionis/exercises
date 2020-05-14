b6c459eb5b3c176139747f719b436dbfb4629528
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-14 11:33:52 +0200

    ale finished

---
+++ 03-PB-console/03console.js
@@ -0,0 +1,21 @@
+console.log('this is an exercise in console loggin');
+let xxYY = 'This is the value.';
+console.log(xxYY);
+
+let firstName = 'alejandra';
+console.log(firstName);
+
+let lastName = 'nope';
+console.log(lastName);
+console.log(firstName, lastName);
+
+let first= 'don';
+let last= 'stella';
+let age = 'ageless';
+let job = 'therapist';
+let city = "universe";
+console.log(first, last, age,job,city);
+
+console.log(first + "is a dog");
+console.log(first, last  + "is an" + age, job + "that lives in this" + city);
+
---
+++ PB-language-statements/variables.js
@@ -0,0 +1,5 @@
+//variables are a BOX / location for storing data or a value.
+//allow computers to store and manipulate data in a dynamic fashion
+//VARIABLE IDENTIFIER: used to name 
+/* JavaScript start with A LETTER !!!
+never use reserved keywords for variable names
