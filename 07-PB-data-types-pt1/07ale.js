e17e39a8de5b46e455a81f6cdfbd44a187fc6ed1
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-18 14:59:21 +0200

    ale finished 07

---
+++ 07-PB-data-types-pt1/07ale.js
@@ -0,0 +1,25 @@
+console.log('question 1');
+let myString = 'i cant stop thinking about gn';
+console.log(myString);
+//i cant stop thinking about gn
+console.log('question 2');
+console.log(myString[2]);
+console.log(myString[6]);
+//answer c & ''
+console.log('question 3-6');
+let name= "gaelle";
+let age= 30;
+let year= 1990;
+console.log(name + "is" + age + "in" + year);
+console.log('question 7-8');
+let isMarried = false;
+console.log(name + "is married" + isMarried);
+//false
+console.log('question 9-11');
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + "is fun" + ":" + isFun);
+console.log(programmingLanguage[0]);
+console.log(programmingLanguage[4]);
+console.log(programmingLanguage.length);
+//length 10
