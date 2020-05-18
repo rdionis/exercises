f65e2023203f37bd17fd510336916c4d919fe5af
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-18 11:56:53 +0200

    ale finished 06

---
+++ 06-PB-ternary-exercises/06ale.js
@@ -0,0 +1,20 @@
+console.log('question 1');
+let isDog = 'dog';
+(isDog=='dog') ? console.log('pat, pat') : console.log('find me dog to pat!');
+//true pat pat
+console.log('question 2');
+let speedCheck = 85;
+(speedCheck => 50) ? console.log('youre going too fast!') : console.log('youre driving below speed limit, oma!');
+//true you're going too fast
+console.log('question 3');
+let age = 53;
+(age < 16) ? console.log('serve butter beer') : console.log('serve beer');
+//serve beer
+console.log('question 4');
+let isStudent = 'student';
+(isStudent = 'student') ? console.log('tickets cost 5,00') : console.log('tickets cost 12,00');
+//student, tickets cost 5,00
+console.log('question 5');
+let okMarie = 'choco';
+(okMarie == 'cake') ? console.log('let them eat cake!') : console.log('oh, bother');
+//oh, bother!
