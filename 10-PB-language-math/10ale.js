f6ed7bf2f7187e16ef0e7d8ebc3de6d568034031
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-20 11:52:09 +0200

    ale finished 10

---
+++ 10-PB-language-math/10ale.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//Q1.a
+console.log(Math.min(-1, 0, 1, 2, 3, 4));
+
+//Q1.b
+console.log(Math.max(-1, 0, 1, 2, 3, 4));
+
+//Q2.
+let a =  3321.32321;
+let b = 326.76;
+let c = 76788.7;
+let d =  -9.78;
+let e = 43.342;
+let f = 28.329;
+
+//b. 
+console.log(Math.ceil(a));
+console.log(Math.ceil(b));
+console.log(Math.ceil(c));
+console.log(Math.ceil(d));
+console.log(Math.ceil(e));
+
+//b.rounding down
+console.log(Math.floor(a));
+console.log(Math.floor(b));
+console.log(Math.floor(c));
+console.log(Math.floor(d));
+console.log(Math.floor(f));
+
+//Q3.
+let random1 = Math.random();
+random1 = random1*10;
+console.log(random1);
+let random2 = Math.ceil(random1);
+console.log(random2);
+
