5bdc057ad12f88a3c92cea97b1c1830116f91b22
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-20 09:36:49 +0200

    ale finished 09

---
+++ 09-PB-language-number/09ale.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//Q1.
+let myString = 'Hi, i\'m Alejandra and i\'m ';
+let myNumber = 101;
+console.log(myString + myNumber); 
+// the + operator combines the 2 variables together 
+
+// Q2.
+let convertString = '1005';
+convertString = parseInt(convertString);
+console.log(convertString);
+
+let convertSecondString = '10.05';
+convertSecondString = parseFloat(convertSecondString);
+console.log(convertSecondString);
+
+// Q3.
+// console.log(30 % 2 = 0 ? '30 is even' : 'is odd');
+let a = 30;
+let b = 939;
+let c = 40.9;
+
+console.log(a % 2 === 0 ? 'is even' : 'is not even');
+console.log(b % 2 === 0 ? 'is even' : 'is not even');
+console.log(c % 2 === 0 ? 'is even' : 'is not even');
+
+
