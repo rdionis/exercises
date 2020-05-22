b12e66b2b9bbc6b111a8b460bfc4fc92b3806c0e
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-22 16:23:43 +0200

    fixed

--- 04-PB-operators-arithmetic-pt2/04ale.js
+++ 04-PB-operators-arithmetic-pt2/04ale.js
@@ -1,29 +1,45 @@
+"use strict";
+//Q1.
 let tiX = 15;
 console.log(6450/tiX);
 //430
+
+//Q2.
 let inCome = 500;
 console.log(inCome * 52.14);
 //26,070
+
+//Q3.
 let percent = 17;
 let total = 30;
 console.log(percent/30 *100);
 //56.666
+
+//Q4.
 let side = 4.75;
 console.log(side*4);
 //19
+
+//Q5.
 let triOne = 5;
 let triTwo = 6; 
 let triThree = 7;
-console.log(triOne+triTwo+triThree);
-//18
+let s = console.log(triOne+triTwo+triThree / 2);
+// 14.5
+
+//Q6.
 let cube = 9;
 console.log(cube*cube*cube);
 //729
-let result = 17/30 * 100;
-console.log(result);
-//
-let bills = 22.35 + (10/100 * 22.35) + 26.67 + (15/100 * 26.67) + 35.92 + (20/100 * 35.92);
-console.log(bills);
+
+//Q7.
+console.log(22.35 + (10/100 * 22.35));
+// ==> 24.585
+console.log(26.67 + (15/100 * 26.67));
+// ==> 30.6705
+console.log(35.92 + (20/100 * 35.92));
+// ==> 43.104
+
 //98.3595
 let mean =  (8+6+5+9+8+2+1+8.5+7+4) / 10;
 console.log(mean);
