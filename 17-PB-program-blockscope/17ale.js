795f566242a37d697678dbfae8fadbf2868db2b5
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-27 15:41:33 +0200

    ale finished 17

---
+++ 17-PB-program-blockscope/17ale.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Q1.
+let d = 103;
+if (d < 0) {
+    let x = "less than 0";
+    console.log(x);
+} else if (d > 0) {
+    let x = "greater than 0";
+    console.log(x);
+}
+
+console.log("question II");
+// Q2.
+let u = 22;
+if (d >= 0) {
+    let u = "greater or equal to 0";
+    let message = "positive integer";
+    console.log(message);
+} if (d < 0) {
+    let u = "less than 0";
+    console.log(u);
+}
