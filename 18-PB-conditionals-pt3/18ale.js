14051311a8e183a2cbab5cc9fdf65b8d46f82cd0
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-05-28 10:57:56 +0200

    ale finished 18

--- 18-PB-conditionals-pt3/18ale.js
+++ 18-PB-conditionals-pt3/18ale.js
@@ -4,17 +4,15 @@
 // 1a.
 let aveA = (79+100+103) / 3;
 let aveL = (116+60+73) / 3;
-console.log(aveA); // 104
-console.log(aveL); // 111
+console.log(aveA); // first time: 104 
+console.log(aveL); // first time: 111
 
 // 1b.
-if (aveA > aveL && aveM) {
+if (aveA > aveL) {
     console.log(`Aleeza with ${aveA} is the winner`);
-} else if (aveL > aveA && aveM) {
+} else if (aveL > aveA) {
     console.log(`Lis with ${aveL} is the winner`);
-} else if (aveM > aveA && aveL) {
-    console.log(`Mary with ${aveM} is the winner`);
-}
+} 
 
 // 1c. changed the scores to make aleeza the winner with 94 average score, instead of 104.
 
@@ -22,7 +20,16 @@ if (aveA > aveL && aveM) {
 let aveM = (97+134+105) / 3;
 console.log(aveM); // 112 --> would it be best practice to put this above? 
 
+
+
 // 1e.
-// see above!
+
+if  ((aveA > aveL) && (aveA > aveM)) {
+    console.log(`Aleeza with ${aveA} is the winner`);
+} else if  ((aveL > aveA) && (aveL > aveM)) {
+    console.log(`Lis with ${aveL} is the winner`);
+} else if  ((aveM > aveA) && (aveM > aveL)) {
+    console.log(`Mary with ${aveM} is the winner`);
+}
 
  
