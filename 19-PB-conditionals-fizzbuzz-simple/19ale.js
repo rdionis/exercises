b25b91b633de4ef46451e414c0098feecfae0652
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-06-02 12:31:16 +0200

    19 fixed

--- 19-PB-conditionals-fizzbuzz-simple/19ale.js
+++ 19-PB-conditionals-fizzbuzz-simple/19ale.js
@@ -7,13 +7,13 @@ let randomNum = Math.floor(Math.random() * (+max - +min)) + +min;
 console.log(randomNum);
 
 
-if (randomNum % 3 === 0) {
+if ((randomNum % 3 === 0) && (randomNum % 5 === 0)) {
+    console.log("FizzBuzz")
+} else if (randomNum % 3 === 0) {
     console.log("Fizz")
 } else if (randomNum % 5 === 0) {
     console.log("Buzz")
-} else if ((randomNum % 3 === 0) && (randomNum % 5 === 0)) {
-    console.log("FizzBuzz")
-} 
+}
 else {
     console.log(randomNum);
 }
