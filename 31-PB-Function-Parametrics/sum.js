2cb715ed432b1df08ad042ffed87a0f132a242a0
DCI-ST151 <a.valencia.urban.nation@gmail.com>
2020-06-16 11:09:54 +0200

    ale finished 31

--- 31-PB-Function-Parametrics/sum.js
+++ 31-PB-Function-Parametrics/sum.js
@@ -1,6 +1,12 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
 
-const sum = () => {};
+const sum = (...args) => {
+    let sum2 = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum2 += args[i];
+    }
+    return sum2;
+};
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
